44fc039a25942486e9063b1b10b5a85c61126f36
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 14:54:52 -0300

    update DisplayTrack.js

--- src/components/DisplayTrack.js
+++ src/components/DisplayTrack.js
@@ -1,7 +1,23 @@
+import { BsMusicNote, BsMusicNoteBeamed } from "react-icons/bs";
+
 const DisplayTrack = ({ currentTrack, audioRef }) => {
   return (
     <div>
       <audio src={currentTrack.src} ref={audioRef} />
+      <div className="audio-info">
+        <div className="audio-image">
+          {currentTrack.thumbnail ? (
+            <img src={currentTrack.thumbnail} alt="audio avatar" />
+          ) : (
+            <div className="icon-wrapper">
+              <span className="audio-icon">
+                <BsMusicNoteBeamed />
+              </span>
+            </div>
+          )}
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 };
