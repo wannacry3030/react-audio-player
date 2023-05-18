d4f8f67721c032209c6c85175baf2a536b4c0a14
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 14:14:22 -0300

    update Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,12 +1,40 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 //icones
 import {
-  
-}
+  IoPlayBackSharp,
+  IoPlayForwardSharp,
+  IoPlaySkipBackwardSharp,
+  IoPlaySkipForwardSharp,
+  IoPlaySkipBackSharp,
+  IoPlaySharp,
+  IoPauseSharp,
+} from "react-icons/io5";
 
 const Controls = () => {
-  return <div>Controls content here</div>;
+  const [isPlaying, setIsPlaying] = useState(false);
+  return (
+    <div className="controls-wrapper">
+      <div className="controls">
+        <button>
+          <IoPlaySkipBackSharp />
+        </button>
+        <button>
+          <IoPlayBackSharp />
+        </button>
+
+        <button>{isPlaying ? <IoPauseSharp /> : <IoPlaySharp />}</button>
+
+        <button>
+          <IoPlayForwardSharp />
+        </button>
+
+        <button>
+          <IoPlaySkipForwardSharp />
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Controls;
