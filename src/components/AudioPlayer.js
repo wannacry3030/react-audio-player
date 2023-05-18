96e4fa613737145ffec397ea0a69bdea825a53fe
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 14:22:57 -0300

    update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -9,11 +9,15 @@ import ProgressBar from "./ProgressBar";
 //renderizando o conteudo da musica na tela do usuario
 const AudioPlayer = () => {
   const [currentTrack, setCurrentTrack] = useState(tracks[0]);
+
+  //referencia
+  const audioRef = useRef();
+
   return (
     <div className="audio-player">
       <div className="inner">
-        <DisplayTrack currentTrack={currentTrack} />
-        <Controls />
+        <DisplayTrack currentTrack={currentTrack} audioRef={audioRef} />
+        <Controls audioRef={audioRef} />
         <ProgressBar />
       </div>
     </div>
