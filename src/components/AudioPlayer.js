f4776dccb92ff8812f5c32f791d9f9c1c2f8b2b5
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 13:20:44 -0300

    update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,1 +1,9 @@
+const AudioPlayer = () => {
+  return (
+    <div className="audio-player">
+      <div className="inner">Audio player content</div>
+    </div>
+  );
+};
 
+export default AudioPlayer;
