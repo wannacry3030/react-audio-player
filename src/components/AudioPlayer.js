04fc51820a93aa2141ed171fdccbc4503dc0a43e
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 13:55:28 -0300

    update AudioPlayer.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,9 +1,14 @@
+import { useState } from 'react'
+import { tracks } from '../data/tracks'
+ 
+//importando os elementos que formam o audio player
 import DisplayTrack from "./DisplayTrack";
 import Controls from "./Controls";
 import ProgressBar from "./ProgressBar";
 
 //renderizando o conteudo da musica na tela do usuario
 const AudioPlayer = () => {
+ const[currentTrack, setCurrentTrack] = useState(tracks[0])
   return (
     <div className="audio-player">
       <div className="inner">
---
+++ src/data/tracks.js
@@ -0,0 +1,14 @@
+//arquivo de uaudio
+import hang from "./Hang_em_all.mp3";
+
+//thumbnail da musica
+import carpenter from "./carpenter.jpg"
+
+import const tracks = [
+  {
+    title: 'Carpenter Brut = Hang em all',
+    src: hang,
+    author 'Carpenter Brut'
+    thumbnail: carpenter
+  }
+]
