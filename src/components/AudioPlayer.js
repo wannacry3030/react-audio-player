937deac2692f3c225d042078505b5a84fff63e36
wannacry3030 <wannacry.bat@proton.me>
2023-05-18 13:57:13 -0300

    update AudioPlayer.js and tracks.js

--- src/components/AudioPlayer.js
+++ src/components/AudioPlayer.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import { tracks } from '../data/tracks'
- 
+import { useState } from "react";
+import { tracks } from "../data/tracks";
+
 //importando os elementos que formam o audio player
 import DisplayTrack from "./DisplayTrack";
 import Controls from "./Controls";
@@ -8,11 +8,11 @@ import ProgressBar from "./ProgressBar";
 
 //renderizando o conteudo da musica na tela do usuario
 const AudioPlayer = () => {
- const[currentTrack, setCurrentTrack] = useState(tracks[0])
+  const [currentTrack, setCurrentTrack] = useState(tracks[0]);
   return (
     <div className="audio-player">
       <div className="inner">
-        <DisplayTrack />
+        <DisplayTrack currentTrack={currentTrack} />
         <Controls />
         <ProgressBar />
       </div>
--- src/data/tracks.js
+++ src/data/tracks.js
@@ -2,13 +2,13 @@
 import hang from "./Hang_em_all.mp3";
 
 //thumbnail da musica
-import carpenter from "./carpenter.jpg"
+import carpenter from "./carpenter.jpg";
 
-import const tracks = [
+export const tracks = [
   {
-    title: 'Carpenter Brut = Hang em all',
+    title: "Carpenter Brut = Hang em all",
     src: hang,
-    author 'Carpenter Brut'
-    thumbnail: carpenter
-  }
-]
+    author: "Carpenter Brut",
+    thumbnail: carpenter,
+  },
+];
