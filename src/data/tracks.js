821363b8301d4a8f5f3c843ec9c231ec4f927390
wannacry3030 <wannacry.bat@proton.me>
2023-05-22 16:37:56 -0300

    update tracks.js

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -4,25 +4,25 @@ import tzu from "./Looking_for_tracy_tzu.mp3";
 import matt from "./Meet_matt_stryker.mp3";
 
 //thumbnail da musica
-import carpenter from "./carpenter.jpg";
+import ram10 from "./ram10.jpeg";
 
 export const tracks = [
   {
     title: "Carpenter Brut = Hang em all",
     src: hang,
     author: "Carpenter Brut",
-    thumbnail: carpenter,
+    thumbnail: ram10,
   },
   {
     title: "Carpenter Brut = Looking for tracy tzu",
     src: tzu,
     author: "Carpenter Brut",
-    thumbnail: carpenter,
+    thumbnail: ram10,
   },
   {
     title: "Carpenter Brut = Meet matt stryker",
     src: matt,
     author: "Carpenter Brut",
-    thumbnail: carpenter,
+    thumbnail: ram10,
   },
 ];
