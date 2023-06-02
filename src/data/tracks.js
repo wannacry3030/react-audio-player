8fe1e93eab1b7abf3a045871e2083bd0e97f5220
wannacry3030 <wannacry.bat@proton.me>
2023-06-02 12:13:54 -0300

    update tracks.js

--- src/data/tracks.js
+++ src/data/tracks.js
@@ -4,6 +4,9 @@ import horizon1 from "./Daft Punk - Horizon Ouverture.mp3";
 import horizoncd from "./Daft Punk - Horizon (Japan CD).mp3";
 import GLBTM from "./Daft Punk - GLBTM (Studio Outtakes).mp3";
 import repeat from "./Daft Punk - Infinity Repeating (2013 Demo) (feat. Julian Casablancas+The Voidz).mp3";
+import gl from "./Daft Punk - GL (Early Take) ft. Pharrell Williams, Nile Rodgers.mp3";
+import prime from "./Daft Punk - Prime (2012 Unfinished).mp3";
+import lytd from "./Daft Punk - LYTD (Vocoder Tests) ft. Pharrell Williams.mp3";
 
 //thumbnail da musica
 import ram10 from "./ram10.jpeg";
@@ -35,14 +38,32 @@ export const tracks = [
     thumbnail: ram10,
   },
   {
-    title: "Horizon (Japan CD)",
-    src: horizoncd,
+    title: "GL (Early Take) ft. Pharrell Williams, Nile Rodgers",
+    src: gl,
     author: "Daft punk",
     thumbnail: ram10,
   },
   {
-    title: "GLBTM (Studio Outtakes)",
-    src: GLBTM,
+    title: "Daft Punk - Prime (2012 Unfinished)",
+    src: prime,
+    author: "Daft punk",
+    thumbnail: ram10,
+  },
+  {
+    title: "LYTD (Vocoder Tests) ft. Pharrell Williams",
+    src: lytd,
+    author: "Daft punk",
+    thumbnail: ram10,
+  },
+  {
+    title: "GL (Early Take) ft. Pharrell Williams, Nile Rodgers",
+    src: gl,
+    author: "Daft punk",
+    thumbnail: ram10,
+  },
+  {
+    title: "Daft Punk - Prime (2012 Unfinished)",
+    src: prime,
     author: "Daft punk",
     thumbnail: ram10,
   },
